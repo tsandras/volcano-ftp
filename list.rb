24f62d83d8731b083b64cbaee701753c2de5ec2c
Faouzi CHAACHOUA <chaach_f@etna-alternance.net>
2014-11-19 11:58:37 +0100

    list + stor

---
+++ list.rb
@@ -0,0 +1,6 @@
+def list_ftp(args)
+    @cs.write "150 Opening data connection for #{args}.\r\n"
+    resp = 'ls -la #{args}'
+    @tsocket.write resp
+    @cs.write "226 Transfer complete.\r\n"
+    @tsocket.close
---
+++ stor.rb
@@ -0,0 +1,8 @@
+def ftp_stor(args)
+    @cs.write "150 Opening ASCII mode data connection for file stor\r\n"
+    file = File.new(args, a)
+    while chunk = @tsocket.read(1024)
+      file.write(chunk)
+    end
+    @cs.write "226 Transfer complete.\r\n"
+
